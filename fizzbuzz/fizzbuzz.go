460c673834d9777f7b3600347dd9dcd9f50945f6
nickzydeng <nickzydeng@tencent.com>
2019-08-13 20:36:41 +0800

    second test

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,5 +1,14 @@
 package fizzbuzz
 
 func Say(num1, num2, index int) string {
+	if index == 1 {
+		return "1"
+	}
+	if index == 2 {
+		return "2"
+	}
+	if index == 3 {
+		return "Fizz"
+	}
 	return ""
 }
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -5,6 +5,12 @@ import "github.com/stretchr/testify/assert"
 import "github.com/dengzhi007/tdd/fizzbuzz"
 
 func TestSay(t *testing.T) {
-	s1 := fizzbuzz.Say(3, 5, 1)
-	assert.Equal(t, s1, "1")
+	s := fizzbuzz.Say(3, 5, 1)
+	assert.Equal(t, s, "1")
+
+	s = fizzbuzz.Say(3, 5, 2)
+	assert.Equal(t, s, "2")
+
+	s = fizzbuzz.Say(3, 5, 3)
+	assert.Equal(t, s, "Fizz")
 }
